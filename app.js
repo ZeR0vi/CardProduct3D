33ba52fb07e717b1dd49d8f5f6e8776a46eeb887
ZeR0 <99215787+ZeR0vi@users.noreply.github.com>
2023-01-11 22:59:14 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const parar = document.getElementById('trocar')
+
+function trade () {
+    document.getElementById('trocar').src = './assets/sofaG.gif'
+}
+
+trocar.addEventListener('click', trade)
+
